9f1f1e4d0dc8b19128b1eeb577fbca9531480a38
Miguel Cruz <macr243@gmail.com>
2020-04-17 16:49:17 -0500

    first version

---
+++ test/Api/PruebaDeSeguridadApiTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace SecurityTestPeru\Client;
+
+class PruebaDeSeguridadApiTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $password = getenv('KEY_PASSWORD');
+        $this->signer = new \SecurityTestPeru\Client\Interceptor\KeyHandler(null, null, $password);
+        $events = new \SecurityTestPeru\Client\Interceptor\MiddlewareEvents($this->signer);
+        $handler = \GuzzleHttp\HandlerStack::create();
+        $handler->push($events->add_signature_header('x-signature'));
+        $handler->push($events->verify_signature_header('x-signature'));
+
+        $client = new \GuzzleHttp\Client(['handler' => $handler]);
+
+        $config = new \SecurityTestPeru\Client\Configuration();
+        $config->setHost('the_url');
+        $this->apiInstance = new \SecurityTestPeru\Client\Api\PruebaDeSeguridadApi($client, $config);
+    }
+
+    public function testSecurityTest()
+    {
+        $x_api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        $body = "Esto es un mensaje de prueba";
+
+        try {
+            $result = $this->apiInstance->securityTest($x_api_key, $body);
+            $this->signer->close();
+            print_r($result);
+        } catch (Exception $e) {
+            echo 'Exception when calling PruebaDeSeguridadApi->securityTest: ', $e->getMessage(), PHP_EOL;
+        }
+    }
+}
